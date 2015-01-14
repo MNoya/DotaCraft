a4035ff475f31dd8b228dab149b0b06e7b4b183e
MNoya <martinnoya@gmail.com>
2015-01-14 16:21:04 -0200

    Finished Demon Hunter

--- scripts/vscripts/heroes/demon_hunter/immolation.lua
+++ scripts/vscripts/heroes/demon_hunter/immolation.lua
@@ -1,29 +1,24 @@
 --[[
-	Author: igo/Noya
-	Date: 13.1.2015.
+	Author: Noya
+	Date: 14.1.2015.
 	Deals AoE damage over time while the ability maintain cost can be paid
 ]]
-function warchasers_blade_berserker_immolation_function( event )
+function Immolation( event )
 	-- Variables
 	local caster = event.caster
 	local ability = event.ability
 	local abilityDamageType = ability:GetAbilityDamageType()
-	local damage_per_second = 5 + 5 * ability:GetLevel()
+	local damage_per_second = ability:GetLevelSpecialValueFor("damage_per_second", ability:GetLevel() - 1 )
 	local manacost_per_second = ability:GetLevelSpecialValueFor("mana_cost_per_second", ability:GetLevel() - 1 )
 	local targets = event.target_entities
 
 	-- Check if the spell mana cost can be maintained
 	if caster:GetMana() >= manacost_per_second then
-		caster:SpendMana( manacost_per_second, event.ability)
+		caster:SpendMana( manacost_per_second, ability)
 
 		-- Deal damage to all targets passed
-		for key, unit in pairs(targets) do
-			ApplyDamage({
-						victim = unit,
-						attacker = caster,
-						damage = damage_per_second,
-						damage_type = abilityDamageType
-						})
+		for _,v in pairs(targets) do
+			ApplyDamage({ victim = v, attacker = caster, damage = damage_per_second, damage_type = abilityDamageType })
 		end
 	else
 		ability:ToggleAbility()
---
+++ scripts/vscripts/heroes/demon_hunter/mana_burn.lua
@@ -0,0 +1,29 @@
+--[[
+	Author: Noya
+	Date: 14.1.2015.
+	Burns mana and deals magic damage equal to the mana burned
+]]
+function ManaBurn( event )
+	-- Variables
+	local target = event.target
+	local caster = event.caster
+	local ability = event.ability
+	local abilityDamageType = ability:GetAbilityDamageType()
+	local mana_burn = ability:GetLevelSpecialValueFor("mana_burn", ability:GetLevel() - 1 )
+	local damage_per_mana = ability:GetLevelSpecialValueFor("damage_per_mana", ability:GetLevel() - 1 )
+	local targetMana = target:GetMana()
+
+	-- Set the new target mana
+	local newMana = targetMana - mana_burn
+	target:SetMana( newMana )
+
+	-- Check how much damage to do
+	-- Discount mana burn over the minimum 0
+	if newMana < 0 then
+		mana_burn = mana_burn + newMana
+	end
+
+	-- Do the damage
+	ApplyDamage({ victim = target, attacker = caster, damage = mana_burn, damage_type = abilityDamageType })
+
+end
