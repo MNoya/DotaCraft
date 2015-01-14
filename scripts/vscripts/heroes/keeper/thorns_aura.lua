89b57d10389b0d82b380f800e38a193b1361fb5a
MNoya <martinnoya@gmail.com>
2015-01-14 18:10:41 -0200

    Finished Keeper Thorns Aura
    Greatly improved the Warchasers code

---
+++ scripts/vscripts/heroes/keeper/thorns_aura.lua
@@ -0,0 +1,28 @@
+--[[
+	Author: Noya
+	Date: 14.1.2015.
+	OnAttacked, register the attacker and the initial HP of the target
+	OnTakeDamage, which is checked after the attack, does the damage by comparing the initial HP to the current health after the damage is dealt
+]]
+function ThornsAura( event )
+	local attacker = event.attacker
+	if not attacker:IsRangedAttacker() then
+		event.target.attacker = event.attacker:GetEntityIndex()
+		event.target.initial_hp = event.target:GetHealth()
+	end
+end
+
+function ThornsAuraDamage( event )
+	local unit = event.unit
+	local attacker = EntIndexToHScript(event.unit.attacker)
+	local ability = event.ability
+	local abilityDamageType = ability:GetAbilityDamageType()
+	local melee_damage_return = ability:GetLevelSpecialValueFor("melee_damage_return", ability:GetLevel() - 1)
+
+	-- Calculate the damage and apply
+	local damage_taken = unit.initial_hp - unit:GetHealth()
+	local return_damage = damage_taken * 0.01 * melee_damage_return
+			
+	ApplyDamage({ victim = attacker, attacker = unit, damage = return_damage, damage_type = abilityDamageType })
+	
+end
