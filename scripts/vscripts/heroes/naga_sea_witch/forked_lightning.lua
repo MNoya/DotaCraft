6936a9df61b194243e671494e9d1a32eb4cd071c
MNoya <martinnoya@gmail.com>
2015-01-17 01:54:56 -0200

    Finished Naga Forked Lightning
    Cone Searching with invisible very fast projectile

---
+++ scripts/vscripts/heroes/naga_sea_witch/forked_lightning.lua
@@ -0,0 +1,33 @@
+--[[
+	Author: Noya
+	Date: 16.01.2015.
+	Shows tornado particles on a target and destroys later
+]]
+function ForkedLightning( event )
+	local hero = event.caster
+	local target = event.target
+	local max_units = event.ability:GetLevelSpecialValueFor("max_units", (event.ability:GetLevel() - 1))
+	
+	if hero.forked_units_hit == nil then
+			hero.forked_units_hit = 0
+	end
+
+	-- hit the target if we haven't hit the max unit count yet
+	if hero.forked_units_hit < max_units then
+		local lightningBolt = ParticleManager:CreateParticle("particles/items_fx/chain_lightning.vpcf", PATTACH_WORLDORIGIN, hero)
+		ParticleManager:SetParticleControl(lightningBolt,0,Vector(hero:GetAbsOrigin().x,hero:GetAbsOrigin().y,hero:GetAbsOrigin().z + hero:GetBoundingMaxs().z ))	
+		ParticleManager:SetParticleControl(lightningBolt,1,Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z + target:GetBoundingMaxs().z ))
+		
+		ApplyDamage({ victim = target,	attacker = hero, damage = event.ability:GetAbilityDamage(),	damage_type = event.ability:GetAbilityDamageType() })
+		EmitSoundOn("Hero_Zuus.ArcLightning.Target", target)
+
+		-- add 1 to the units hit
+		hero.forked_units_hit = hero.forked_units_hit + 1
+	else
+		-- Wait for the spell to end
+		Timers:CreateTimer(0.5, function()
+			hero.forked_units_hit = nil
+		end)
+	end
+
+end
