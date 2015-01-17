1ae172e0a532b32eed84d5ae7644b4f567d00a77
MNoya <martinnoya@gmail.com>
2015-01-16 22:48:59 -0200

    Finished Tornado

---
+++ scripts/vscripts/heroes/naga_sea_witch/tornado.lua
@@ -0,0 +1,26 @@
+--[[
+	Author: Noya
+	Date: 16.01.2015.
+	Shows tornado particles on a target and destroys later
+]]
+function TornadoParticle(event)
+	local target = event.target
+	target.tornado = ParticleManager:CreateParticle("particles/neutral_fx/tornado_ambient.vpcf", PATTACH_WORLDORIGIN, event.caster)
+	ParticleManager:SetParticleControl(target.tornado, 0, Vector(target:GetAbsOrigin().x,target:GetAbsOrigin().y,target:GetAbsOrigin().z - 50 ))
+end
+
+function EndTornadoParticle(event)
+	local target = event.target
+	ParticleManager:DestroyParticle(target.tornado,false)
+end
+
+--[[
+	Author: Noya
+	Date: 16.01.2015.
+	Rotates by an angle degree
+]]
+function Spin(keys)
+    local target = keys.target
+    local total_degrees = keys.Angle
+    target:SetForwardVector(RotatePosition(Vector(0,0,0), QAngle(0,total_degrees,0), target:GetForwardVector()))
+end
