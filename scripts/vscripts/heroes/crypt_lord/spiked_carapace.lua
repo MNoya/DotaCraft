834f6094dfa0add05ee3ba9387af886fe77628f3
MNoya <martinnoya@gmail.com>
2015-02-01 13:53:29 -0200

    Improved Return Damage method to use attack_damage

--- scripts/vscripts/heroes/crypt_lord/spiked_carapace.lua
+++ scripts/vscripts/heroes/crypt_lord/spiked_carapace.lua
@@ -1,29 +1,21 @@
 --[[
 	Author: Noya
-	Date: 17.1.2015.
-	OnAttacked, register the attacker and the initial HP of the target
-	OnTakeDamage, which is checked after the attack, does the damage by comparing the initial HP to the current health after the damage is dealt
+	Date: 01.02.2015.
+	OnAttacked, deal damage based on the attack_damage, only to melee attackers
 ]]
 function SpikedCarapace( event )
 	local attacker = event.attacker
-	if not attacker:IsRangedAttacker() then
-		event.target.attacker = event.attacker:GetEntityIndex()
-		event.target.initial_hp = event.target:GetHealth()
-	end
-end
-
-function SpikedCarapaceDamage( event )
-	local unit = event.unit
-	local attacker = EntIndexToHScript(event.unit.attacker)
+	local target = event.target --the damaged unit
 	local ability = event.ability
 	local abilityDamageType = ability:GetAbilityDamageType()
 	local melee_damage_return = ability:GetLevelSpecialValueFor("melee_damage_return", ability:GetLevel() - 1)
-
-	-- Calculate the damage and apply
-	local damage_taken = unit.initial_hp - unit:GetHealth()
+	local damage_taken = event.Damage
 	local return_damage = damage_taken * 0.01 * melee_damage_return
-			
-	ApplyDamage({ victim = attacker, attacker = unit, damage = return_damage, damage_type = abilityDamageType })
-	print(damage_taken,return_damage)
-	
+
+	-- Apply the damage only to ranged attacker
+	if not attacker:IsRangedAttacker() then
+
+		ApplyDamage({ victim = attacker, attacker = target, damage = return_damage, damage_type = abilityDamageType })
+		print(damage_taken,return_damage)
+	end
 end
