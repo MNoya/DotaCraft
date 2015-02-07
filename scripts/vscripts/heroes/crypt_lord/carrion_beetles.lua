e516e2f6b1e0cb886f7afa46fa3bb9169c863b2f
MNoya <martinnoya@gmail.com>
2015-02-07 21:22:58 -0200

    Added Burrow to Crypt Lord Beetles

--- scripts/vscripts/heroes/crypt_lord/carrion_beetles.lua
+++ scripts/vscripts/heroes/crypt_lord/carrion_beetles.lua
@@ -69,3 +69,28 @@ function RemoveDeadBeetle( event )
     	end
 	end
 end
+
+--[[
+	Author: Noya
+	Date: 07.02.2015.
+	Burrows Up or Down
+]]
+function Burrow( event )
+	local caster = event.caster
+	local move = event.Move
+	local caster_x = event.caster:GetAbsOrigin().x
+	local caster_y = event.caster:GetAbsOrigin().y
+	local caster_z = event.caster:GetAbsOrigin().z
+	local position = Vector(caster_x, caster_y, caster_z)
+
+	if move == "up" then
+		position.z = position.z + 128
+		caster:SetAbsOrigin(position)
+		print(position)
+	else
+		position.z = position.z - 128
+		caster:SetAbsOrigin(position)
+		print(position)
+	end
+
+end
