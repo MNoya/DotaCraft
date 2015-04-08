932ee85ff82fcdf1e6c63927bba854ec1b0dc4e4
MNoya <martinnoya@gmail.com>
2015-04-08 00:48:41 -0300

    Aerial Shackles finished

--- scripts/vscripts/units/human/dragonhawk_rider.lua
+++ scripts/vscripts/units/human/dragonhawk_rider.lua
@@ -48,3 +48,41 @@ function ApplyCloud( event )
 		end
 	end
 end
+
+-- Prevents casting shackles on anything that doesnt fly
+function AerialShacklesCheck( event )
+	local caster = event.caster
+	local pID = caster:GetOwner():GetPlayerID()
+	local target = event.target
+
+	if not target:HasFlyMovementCapability() then
+		caster:Interrupt()
+		FireGameEvent( 'custom_error_show', { player_ID = pID, _error = "Ability Needs To Target Air Unit" } )
+	end
+end
+
+-- Loses flying capability
+function LoseFlying( event )
+	local target = event.target
+	target:SetMoveCapability(DOTA_UNIT_CAP_MOVE_GROUND)
+end
+
+-- Moves down a bit
+function LoseHeight( event )
+	local target = event.target
+	local origin = target:GetAbsOrigin()
+	local groundPos = GetGroundPosition(origin, target)
+
+	print(origin.z, groundPos.z)
+	if origin.z+128 > groundPos.z then
+		target:SetAbsOrigin(Vector(origin.x, origin.y, origin.z - 2))
+	end
+end
+
+-- Gains flying capability
+function ReGainFlying( event )
+	local target = event.target
+	local origin = target:GetAbsOrigin()
+	
+	target:SetMoveCapability(DOTA_UNIT_CAP_MOVE_FLY)
+end
