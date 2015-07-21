0069268ec7784ee277f8f16f9f9a099b6fef6a90
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-07-20 22:36:48 -0300

    Added pillage for orc units

---
+++ game/dota_addons/dotacraft/scripts/vscripts/units/orc/pillage.lua
@@ -0,0 +1,22 @@
+function Pillage(event)
+
+	local target = event.target
+	local caster = event.caster
+
+	if not IsCustomBuilding(target) then
+		return
+	end
+
+	if caster.pillaged_gold == nil then
+		caster.pillaged_gold = 0
+	end
+
+	local pillage = (event.attack_damage/target:GetMaxHealth()) * event.pillage_ratio * GetGoldCost(target)
+	caster.pillaged_gold = caster.pillaged_gold + pillage
+	local effective_gold = math.floor(caster.pillaged_gold)
+
+	if effective_gold > 0 then
+		caster:ModifyGold(effective_gold, false, 0)
+		caster.pillaged_gold = caster.pillaged_gold - effective_gold
+	end
+end
