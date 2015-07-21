faba8cc30fe2719ad4ddcac88d946c0a1bea09fe
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-07-21 16:00:37 -0300

    Added damage calculation to pillage

--- game/dota_addons/dotacraft/scripts/vscripts/units/orc/pillage.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/units/orc/pillage.lua
@@ -11,13 +11,26 @@ function Pillage(event)
 		caster.pillaged_gold = 0
 	end
 
-	local pillage = (event.attack_damage/target:GetMaxHealth()) * event.pillage_ratio * GetGoldCost(target)
+	local damage = event.attack_damage
+	local unitName = caster:GetUnitName()
+
+	if unitName == 'orc_raider' then
+		damage = damage * 1.5
+	else
+		damage = damage * 0.7
+	end
+
+	local armor = target:GetPhysicalArmorValue()
+
+	damage = damage * (1 - 0.06 * armor / (1 + 0.06 * math.abs(armor)))
+
+	local pillage = (damage/target:GetMaxHealth()) * event.pillage_ratio * GetGoldCost(target)
 	caster.pillaged_gold = caster.pillaged_gold + pillage
 	local effective_gold = math.floor(caster.pillaged_gold)
 
 	if effective_gold > 0 then
 		caster:GetPlayerOwner():GetAssignedHero():ModifyGold(effective_gold, false, 0)
-		ParticleManager:CreateParticle(particle, PATTACH_OVERHEAD_FOLLOW, caster)
+		ParticleManager:CreateParticle("particles/units/heroes/hero_alchemist/alchemist_lasthit_coins.vpcf", PATTACH_OVERHEAD_FOLLOW, caster)
 		PopupGoldGain(caster, effective_gold)
 		caster.pillaged_gold = caster.pillaged_gold - effective_gold
 	end
