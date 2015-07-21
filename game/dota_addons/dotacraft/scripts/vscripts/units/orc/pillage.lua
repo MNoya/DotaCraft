a29da86969a4ddc0e02c708804ceb588873f9124
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-07-21 02:14:51 -0300

    Removed debug msgs, and fixed Gold gain

--- game/dota_addons/dotacraft/scripts/vscripts/units/orc/pillage.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/units/orc/pillage.lua
@@ -1,14 +1,10 @@
 function Pillage(event)
-	print("Hello from Pillage")
 	local target = event.target
 	local caster = event.caster
 
 	if not IsCustomBuilding(target) then
-		print("Not a custom building being hit, bye")
 		return
 	end
-
-	print("It's a custom building!")
 
 	if caster.pillaged_gold == nil then
 		caster.pillaged_gold = 0
@@ -17,10 +13,9 @@ function Pillage(event)
 	local pillage = (event.attack_damage/target:GetMaxHealth()) * event.pillage_ratio * GetGoldCost(target)
 	caster.pillaged_gold = caster.pillaged_gold + pillage
 	local effective_gold = math.floor(caster.pillaged_gold)
-	print("pillaged gold: " .. pillage)
 
 	if effective_gold > 0 then
-		caster:ModifyGold(effective_gold, false, 0)
+		caster:GetPlayerOwner():GetAssignedHero():ModifyGold(effective_gold, false, 0)
 		PopupGoldGain(caster, effective_gold)
 		caster.pillaged_gold = caster.pillaged_gold - effective_gold
 	end
