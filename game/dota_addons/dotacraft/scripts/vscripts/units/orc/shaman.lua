d9cdcc085ec8fc7dd864c45da856681c6d16ef42
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-07-23 17:31:27 -0300

    Finished Shaman's Bloodlust

--- game/dota_addons/dotacraft/scripts/vscripts/units/orc/shaman.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/units/orc/shaman.lua
@@ -1,7 +1,16 @@
 function Bloodlust(event)	
+	local caster = event.caster
 	local target = event.target
 	local ability = event.ability
 	local modelScale = 1 + ability:GetLevelSpecialValueFor('scaling_factor', 0)
+	caster:EmitSound('Hero_OgreMagi.Bloodlust.Cast')
+	target:EmitSound('Hero_OgreMagi.Bloodlust.Target')
 	target:SetModelScale(modelScale)
 	ability:ApplyDataDrivenModifier(caster, target, 'modifier_orc_bloodlust', nil) 
+end
+
+function BloodlustDelete(event)	
+	local target = event.target
+	local modelScale = 1
+	target:SetModelScale(modelScale)
 end
