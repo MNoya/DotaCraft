c30800fa92b061d3f1490665c278dce9671516b5
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-07-25 15:56:27 -0300

    Orc's Lightning shield and Bloodlust Finished

--- game/dota_addons/dotacraft/scripts/vscripts/units/orc/shaman.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/units/orc/shaman.lua
@@ -1,16 +1,86 @@
+--[[ ============================================================================================================
+	Author: wFX, based on Noya/Rook code.
+================================================================================================================= ]]
+
 function Bloodlust(event)	
 	local caster = event.caster
 	local target = event.target
 	local ability = event.ability
-	local modelScale = 1 + ability:GetLevelSpecialValueFor('scaling_factor', 0)
+
+	if target.bloodlust_timer then
+		Timers:RemoveTimer(target.bloodlust_timer)
+	end
+
+	local scaling_factor = ability:GetLevelSpecialValueFor('scaling_factor', 0)
+	local final_model_scale = GameRules.UnitKV[target:GetUnitName()]["ModelScale"] + scaling_factor
+	local model_size_interval = scaling_factor/25
+	local interval = 0.03
+	target.bloodlust_timer = Timers:CreateTimer(interval, function() 
+			local current_scale = target:GetModelScale()
+			if current_scale <= final_model_scale then
+				local modelScale = current_scale + model_size_interval
+				target:SetModelScale( modelScale )
+				return 0.03
+			else
+				return
+			end
+		end)
+
+	ability:ApplyDataDrivenModifier(caster, target, 'modifier_orc_bloodlust', nil) 
+
 	caster:EmitSound('Hero_OgreMagi.Bloodlust.Cast')
 	target:EmitSound('Hero_OgreMagi.Bloodlust.Target')
-	target:SetModelScale(modelScale)
-	ability:ApplyDataDrivenModifier(caster, target, 'modifier_orc_bloodlust', nil) 
 end
 
 function BloodlustDelete(event)	
 	local target = event.target
-	local modelScale = 1
-	target:SetModelScale(modelScale)
+	local ability = event.ability
+	local scaling_factor = ability:GetLevelSpecialValueFor('scaling_factor', 0)
+	local final_model_scale = GameRules.UnitKV[target:GetUnitName()]["ModelScale"]
+	local model_size_interval = scaling_factor/50
+	
+	if target.bloodlust_timer then
+		Timers:RemoveTimer(target.bloodlust_timer)
+	end
+	local interval = 0.03
+	target.bloodlust_timer = Timers:CreateTimer(interval, function() 
+			local current_scale = target:GetModelScale()
+			if current_scale >= final_model_scale then
+				local modelScale = current_scale - model_size_interval
+				target:SetModelScale( modelScale )
+				return 0.03
+			else
+				return
+			end
+		end)
+
+end
+
+
+function LightningShieldOnSpellStart(event)
+	local caster = event.caster
+	local ability = event.ability
+	local target = event.target
+	local duration = ability:GetLevelSpecialValueFor("duration", 0)
+	target:EmitSound("Hero_Zuus.StaticField")
+	ability:ApplyDataDrivenModifier(caster, target, 'modifier_orc_lightning_shield', nil)		
+end
+
+function ModifierLightningShieldOnIntervalThink(event)
+	local caster = event.caster
+	local target = event.target
+	local ability = event.ability
+	local radius = ability:GetLevelSpecialValueFor("radius", 0)
+	local dps = ability:GetLevelSpecialValueFor("damage_per_second", 0)
+	local factor = ability:GetLevelSpecialValueFor("think_interval", 0)
+	local damage = dps*factor
+
+	local nearby_units = FindUnitsInRadius(caster:GetTeam(), target:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_BOTH,
+			DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+	
+	for i, nUnit in ipairs(nearby_units) do
+		if target ~= nUnit then  --The carrier of Lightning Shield cannot damage itself.
+			ApplyDamage({victim = nUnit, attacker = caster:GetOwner(), damage = damage, damage_type = DAMAGE_TYPE_MAGICAL})
+		end
+	end
 end
