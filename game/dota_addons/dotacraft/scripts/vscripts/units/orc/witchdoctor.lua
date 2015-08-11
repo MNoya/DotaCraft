65a2a3ad7fda7611cb2ff523cdffc53c3342cecb
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-08-11 19:07:23 -0300

    Minor fix to sentry ward

--- game/dota_addons/dotacraft/scripts/vscripts/units/orc/witchdoctor.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/units/orc/witchdoctor.lua
@@ -16,7 +16,7 @@ function SentrySight( event )
 	local ability = event.ability
 	caster:AddNewModifier(caster, nil, "modifier_invisible", {duration = 1})
 	local radius = ability:GetSpecialValueFor('radius') 
-	local enemies = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO, 0, FIND_ANY_ORDER, false)
+	local enemies = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, 0, FIND_ANY_ORDER, false)
 	for _,enemy in pairs(enemies) do
 		enemy:AddNewModifier(caster, ability, 'modifier_truesight', {duration = '0.5'}) 
 	end
