6a6f465500176035f216fbb1bd05900a0c2d1454
Infekma <de_atd@hotmail.com>
2015-08-18 16:36:15 +0100

    Loading screen script file update
    incorrectly used (and forgot) Math.random()

--- content/dota_addons/dotacraft/panorama/scripts/custom_loading_screen.js
+++ content/dota_addons/dotacraft/panorama/scripts/custom_loading_screen.js
@@ -1,7 +1,7 @@
 // choose random hint
 function Choose_Hint(){
-	var count = Count_Localized_Strings()
-	var random_number = Math.round(count)
+	var count =  Count_Localized_Strings()
+	var random_number = Math.floor(Math.random() * count)
 	
 	// accepted random localized string
 	var Localized_String = $.Localize("loading_screen_hint_"+random_number)
@@ -10,16 +10,22 @@ function Choose_Hint(){
 	Hint_Text.text = "Hint: "+Localized_String
 }
 
-// an infinite loop that stops once an unlocalised string is found
+// an infinite loop that stops once an u nlocalised string is found
+// function assume that atleast a hint index _0 EXIST
 function Count_Localized_Strings(){
+	if($.Localize("loading_screen_hint_0") == "loading_screen_hint_0"){
+		$.Msg("[LOADING SCREEN] No localized string found, or incorrectly indexed(start at 0)")
+		return
+	}
+	
 	for(i=0; i == i; i++){	
 		// store localized string based on i Index
-		var Localized_String = $.Localize("loading_screen_hint_"+i)
+		var Localized_String = $.Localize("loading_screen_hint_"+(i+1))
 		
 		// check if the localized string is identical to the localize value, if true that means there's no localisation for this, we assume at least.
-		if(Localized_String == "loading_screen_hint_"+i){
-			$.Msg("BREAKING OUT OF COUNT, COUNT="+i)
-			return y = i -1
+		if(Localized_String == "loading_screen_hint_"+(i+1)){
+			//$.Msg("BREAKING OUT OF COUNT, COUNT="+i)
+			return i
 			break;
 		}
 	}
