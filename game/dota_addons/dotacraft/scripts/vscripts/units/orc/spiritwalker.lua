7534f3ab03ec23162e8403770c267d56dfcd7049
MNoya <martinnoya@gmail.com>
2015-08-22 14:27:31 -0300

    Disenchant parity, pulverize, envenomed weapons

--- game/dota_addons/dotacraft/scripts/vscripts/units/orc/spiritwalker.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/units/orc/spiritwalker.lua
@@ -15,7 +15,7 @@ function EtherStart( event )
 	end
 end
 
-function DispelMagic( event )
+function Disenchant( event )
 	local caster = event.caster
 	local ability = event.ability
 	local point = event.target_points[1]
@@ -24,36 +24,22 @@ function DispelMagic( event )
 	-- Find targets in radius
 	local units = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false)
 	for k,unit in pairs(units) do
-		event.target = unit
-		Dispel(event)
+		local bSummon = unit:IsDominated() or unit:HasModifier("modifier_kill")
+		if bSummon then
+			local damage_to_summons = event.ability:GetSpecialValueFor("damage_to_summons")
+			ApplyDamage({victim = unit, attacker = caster, damage = damage_to_summons, damage_type = DAMAGE_TYPE_PURE})
+			ParticleManager:CreateParticle("particles/econ/items/enchantress/enchantress_lodestar/ench_death_lodestar_burst.vpcf", PATTACH_ABSORIGIN_FOLLOW, unit)
+		end
+
+		-- This ability removes both positive and negative buffs from units.
+		local bRemovePositiveBuffs = true
+		local bRemoveDebuffs = true
+		local bFrameOnly = false
+		local bRemoveStuns = false
+		local bRemoveExceptions = false
+
+		unit:Purge(bRemovePositiveBuffs, bRemoveDebuffs, bFrameOnly, bRemoveStuns, bRemoveExceptions)
 	end
 
 	RemoveBlight(point, radius)
-end
-
-function Dispel( event )
-	local caster = event.caster
-	local target = event.target
-
-	local bSummon = target:IsDominated() or target:HasModifier("modifier_kill")
-	if bSummon then
-		local damage_to_summons = event.ability:GetSpecialValueFor("damage_to_summons")
-		ApplyDamage({victim = target, attacker = caster, damage = damage_to_summons, damage_type = DAMAGE_TYPE_PURE})
-		ParticleManager:CreateParticle("particles/econ/items/enchantress/enchantress_lodestar/ench_death_lodestar_burst.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
-	end
-
-	local bRemovePositiveBuffs = false
-	local bRemoveDebuffs = false
-	local bFrameOnly = false
-	local bRemoveStuns = false
-	local bRemoveExceptions = false
-
-	-- Remove buffs on enemies or debuffs on allies
-	if target:GetTeamNumber() ~= caster:GetTeamNumber() then
-		bRemovePositiveBuffs = true
-	else
-		bRemoveDebuffs = true
-	end
-
-	target:Purge(bRemovePositiveBuffs, bRemoveDebuffs, bFrameOnly, bRemoveStuns, bRemoveExceptions)
 end
