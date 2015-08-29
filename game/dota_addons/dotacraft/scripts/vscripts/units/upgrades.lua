6c33c70343462b9715fed8512a94ec497e1d459c
MNoya <martinnoya@gmail.com>
2015-08-29 13:07:02 -0300

    Generic backpack

--- game/dota_addons/dotacraft/scripts/vscripts/units/upgrades.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/units/upgrades.lua
@@ -46,3 +46,47 @@ function ApplyTraining( event )
 
 	UpgradeWearables(caster, training_level, "training")
 end
+
+-- Gives an inventory to this unit
+function Backpack( event )
+    local caster = event.caster
+    local action = event.Action
+
+    if action == "Enable" then
+        caster:SetHasInventory(true)
+        -- Remove 2 backpack slot items
+        for itemSlot = 0, 1 do
+            local item = caster:GetItemInSlot( itemSlot )
+            if item then 
+                caster:RemoveItem(item)
+            end
+        end
+    else
+        caster:SetHasInventory(false)
+        -- Make 6 undroppable backpack items
+        for itemSlot = 0, 5 do
+            local newItem = CreateItem("item_backpack", nil, nil)
+            caster:AddItem(newItem)
+        end
+    end
+end
+
+-- Drop all the items on the killed unit
+function BackpackDrop( event )
+    local caster = event.caster
+    local position = caster:GetAbsOrigin()
+
+    for itemSlot = 0, 5 do
+        local item = caster:GetItemInSlot( itemSlot )
+        if item and item:IsDroppable() then
+            local itemName = item:GetAbilityName()
+            local newItem = CreateItem(itemName, nil, nil)
+            local drop = CreateItemOnPositionSync( position , newItem)
+            if drop then
+                drop:SetContainedItem( newItem )
+                newItem:LaunchLoot( false, 100, 0.35, position + RandomVector( RandomFloat( 10, 100 ) ) )
+            end
+            caster:RemoveItem(item)
+        end
+    end
+end
