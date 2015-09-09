ad7815d3cd4abfefdaedf5eb3ec394518ac6ade0
MNoya <martinnoya@gmail.com>
2015-09-09 12:55:25 -0300

    Rewritten Death Coil and Holy Light to ability_lua
    Fixes #16

--- game/dota_addons/dotacraft/scripts/vscripts/heroes/death_knight/death_coil.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/death_knight/death_coil.lua
@@ -1,26 +1,110 @@
 --[[
 	Author: Noya
-	Date: 21.01.2015.
-	Kills a target, gives Health to the caster according to the sacrificed target current Health
+	Date: 9 September 2015
+	Can only be cast on living allied units (Heal) or enemy Undead units (Damage). 
+	Disallows self targeting and casting on allied units with full health (serverside only)
 ]]
-function DeathCoil( event )
-	local caster = event.caster
-	local target = event.target
-	local ability = event.ability
+death_knight_death_coil = class({})
+
+function death_knight_death_coil:OnSpellStart()
+	local ability = self
+	local caster = ability:GetCaster()
+	local target = ability:GetCursorTarget()
+
+	local projectile_speed = ability:GetSpecialValueFor("projectile_speed")
+	local projectile_name = "particles/custom/vengeful_magic_missle.vpcf"
+
+	local projectile = {
+		Target = target,
+		Source = caster,
+		Ability = ability,
+		EffectName = projectile_name,
+		bDodgable = true,
+		bProvidesVision = false,
+		iMoveSpeed = projectile_speed,
+		iVisionTeamNumber = caster:GetTeamNumber(),
+		iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
+	}
+
+	ProjectileManager:CreateTrackingProjectile(projectile)
+
+	caster:EmitSound("Hero_Abaddon.DeathCoil.Cast")
+end
+
+function death_knight_death_coil:OnProjectileHit( target, location )
+	local ability = self
+	local caster = ability:GetCaster()
+	
 	local damage = ability:GetLevelSpecialValueFor( "target_damage" , ability:GetLevel() - 1 )
 	local heal = ability:GetLevelSpecialValueFor( "heal_amount" , ability:GetLevel() - 1 )
+	
 	if target:GetTeamNumber() ~= caster:GetTeamNumber() then
 		ApplyDamage({ victim = target, attacker = caster, damage = damage, damage_type = DAMAGE_TYPE_MAGICAL })
 	else
-		target:Heal( heal, caster)
+		local healDone = math.min(heal,target:GetHealthDeficit())
+		if healDone > 0 then
+			PopupHealing(target, healDone)
+			target:Heal( heal, caster)
+		end
 	end
+
+	target:EmitSound("Hero_Abaddon.DeathCoil.Target")
 end
 
--- Denies self cast, with a message
-function DeathCoilPrecast( event )
-	local pID = event.caster:GetPlayerID()
-	if event.target == event.caster then
-		event.caster:Interrupt()
-		SendErrorMessage(pID, "#error_cant_target_self")
+--------------------------------------------------------------------------------
+
+function death_knight_death_coil:CastFilterResultTarget( target )
+	local ability = self
+	local caster = self:GetCaster()
+
+	-- Check Undead for allies or Living for enemies
+	local casterTeam = caster:GetTeamNumber()
+ 	local targetTeam = target:GetTeamNumber()
+ 	local allied = casterTeam == targetTeam
+ 	local bUndead = string.match(target:GetUnitName(),"undead")
+
+	-- Check self-target
+	if caster == target then 
+		return UF_FAIL_CUSTOM
 	end
+
+	-- Check full health ally
+	if IsServer() then
+		if allied and target:GetHealthDeficit() == 0 then
+			return UF_FAIL_CUSTOM
+		end
+	end
+
+ 	-- Prevent healing living allies or damaging undead enemies
+ 	if (allied and not bUndead) or (not allied and bUndead) then
+ 		return UF_FAIL_CUSTOM
+ 	end
+
+	return UF_SUCCESS
+end
+  
+function death_knight_death_coil:GetCustomCastErrorTarget( target )
+	local ability = self
+	local caster = self:GetCaster()
+
+	local casterTeam = caster:GetTeamNumber()
+ 	local targetTeam = target:GetTeamNumber()
+ 	local allied = casterTeam == targetTeam
+ 	local bUndead = string.match(target:GetUnitName(),"undead")
+
+	if caster == target then
+		return "#error_cant_target_self"
+	end
+
+	if IsServer() then
+		if allied and target:GetHealthDeficit() == 0 then
+			return "#error_full_health"
+		end
+	end
+
+ 	if (allied and not bUndead) or (not allied and bUndead) then
+ 		return "#error_must_target_undead_allies"
+ 	end
+ 
+	return ""
 end
--- game/dota_addons/dotacraft/scripts/vscripts/heroes/paladin/holy_light.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/paladin/holy_light.lua
@@ -1,44 +1,91 @@
 --[[
 	Author: Noya
-	Date: 13.1.2015.
-	Disallows self targeting. If cast on an ally it will heal, if cast on an enemy it will do damage.
-	Doesn't have the Undead restriction for now
+	Date: 9 September 2015
+	Can only be cast on living allied units (Heal) or enemy Undead units (Damage)
+	Disallows self targeting and casting on allied units with full health (serverside only)
 ]]
-function HolyLight( event )
-	-- Variables
-	local caster = event.caster
-	local target = event.target
-	local ability = event.ability
+paladin_holy_light = class({})
+
+function paladin_holy_light:OnSpellStart( event )
+	local ability = self
+	local caster = ability:GetCaster()
+	local target = ability:GetCursorTarget()
+
 	local damage = ability:GetLevelSpecialValueFor( "target_damage" , ability:GetLevel() - 1  )
 	local heal = ability:GetLevelSpecialValueFor( "heal_amount" , ability:GetLevel() - 1 )
 	local particle_radius = ability:GetLevelSpecialValueFor( "particle_radius" , ability:GetLevel() - 1 )
 	local particle_name = "particles/units/heroes/hero_omniknight/omniknight_purification.vpcf"
 
-	-- Check self-target
-	if caster ~= target then 
-		-- Play the ability sound
-		target:EmitSound("Hero_Omniknight.Purification")
+	-- Play the ability sound
+	target:EmitSound("Hero_Omniknight.Purification")
 
-		-- If the target and caster are on a different team, do Damage. Heal otherwise
-		if target:GetTeamNumber() ~= caster:GetTeamNumber() then
-			ApplyDamage({ victim = target, attacker = caster, damage = damage,	damage_type = DAMAGE_TYPE_MAGICAL })
-		else
+	-- If the target and caster are on a different team, do Damage. Heal otherwise
+	if target:GetTeamNumber() ~= caster:GetTeamNumber() then
+		ApplyDamage({ victim = target, attacker = caster, damage = damage,	damage_type = DAMAGE_TYPE_MAGICAL })
+	else
+		local healDone = math.min(heal,target:GetHealthDeficit())
+		if healDone > 0 then
+			PopupHealing(target, healDone)
 			target:Heal( heal, caster)
 		end
-
-		-- Particle 
-		local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_omniknight/omniknight_purification.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
-		ParticleManager:SetParticleControl(particle, 0, target:GetAbsOrigin())
-		ParticleManager:SetParticleControl(particle, 1, Vector(particle_radius,0,0))
-
-	else
-		--The ability was self targeted, refund mana and cooldown
-		ability:RefundManaCost()
-		ability:EndCooldown()
-
-		-- Play Error Sound
-		EmitSoundOnClient("General.CastFail_InvalidTarget_Hero", caster:GetPlayerOwner())
-
-		SendErrorMessage(pID, "#error_cant_target_self")
 	end
+
+	-- Particle 
+	local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_omniknight/omniknight_purification.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+	ParticleManager:SetParticleControl(particle, 0, target:GetAbsOrigin())
+	ParticleManager:SetParticleControl(particle, 1, Vector(particle_radius,0,0))
+end
+
+--------------------------------------------------------------------------------
+ 
+function paladin_holy_light:CastFilterResultTarget( target )
+	local ability = self
+	local caster = ability:GetCaster()
+
+	-- Check Undead for allies or Living for enemies
+	local casterTeam = caster:GetTeamNumber()
+ 	local targetTeam = target:GetTeamNumber()
+ 	local allied = casterTeam == targetTeam
+ 	local bUndead = string.match(target:GetUnitName(),"undead")
+
+	-- Check self-target
+	if caster == target then 
+		return UF_FAIL_CUSTOM
+	end
+
+	-- Check full health ally
+	if allied and target:GetHealthDeficit() == 0 then
+		return UF_FAIL_CUSTOM
+	end
+
+ 	-- Prevent healing undead allies or damaging non undead enemies
+ 	if (allied and bUndead) or (not allied and not bUndead) then
+ 		return UF_FAIL_CUSTOM
+ 	end
+
+	return UF_SUCCESS
+end
+  
+function paladin_holy_light:GetCustomCastErrorTarget( target )
+	local ability = self
+	local caster = self:GetCaster()
+
+	local casterTeam = caster:GetTeamNumber()
+ 	local targetTeam = target:GetTeamNumber()
+ 	local allied = casterTeam == targetTeam
+ 	local bUndead = string.match(target:GetUnitName(),"undead")
+
+	if caster == target then
+		return "#error_cant_target_self"
+	end
+
+ 	if allied and target:GetHealthDeficit() == 0 then
+		return "#error_full_health"
+	end
+
+ 	if (allied and bUndead) or (not allied and not bUndead) then
+ 		return "#error_cant_target_undead_allies"
+ 	end
+ 
+	return ""
 end
