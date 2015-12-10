4c7de81a3c90ad6315d1904ab019d6e4a35e11b8
MNoya <martinnoya@gmail.com>
2015-12-10 14:11:00 -0300

    Fixed #106 and disabled mana burn through magic immunity

--- game/dota_addons/dotacraft/scripts/vscripts/units/human/spell_breaker.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/units/human/spell_breaker.lua
@@ -1,7 +1,8 @@
---[[Mana drain and damage part of Mana Break
-	Author: Pizzalol
-	Date: 16.12.2014.
-	NOTE: Currently works on magic immune enemies, can be fixed by checking for magic immunity before draining mana and dealing damage]]
+--[[
+	Mana drain, damage and effects, ignoring magic immunity
+	Author: Noya, Pizzalol
+	Date: December 10, 2015
+]]
 function ManaBreak( keys )
 	local target = keys.target
 	local caster = keys.caster
@@ -9,12 +10,14 @@ function ManaBreak( keys )
 	local manaBurn = ability:GetLevelSpecialValueFor("mana_per_hit", (ability:GetLevel() - 1))
 	local manaDamage = ability:GetLevelSpecialValueFor("damage_per_burn", (ability:GetLevel() - 1))
 
+	if target:IsMagicImmune() then return end
+
 	local damageTable = {}
 	damageTable.attacker = caster
 	damageTable.victim = target
 	damageTable.damage_type = ability:GetAbilityDamageType()
 	damageTable.ability = ability
-	damageTable.damage_flags = DOTA_UNIT_TARGET_FLAG_NONE -- Doesnt seem to work?
+	damageTable.damage_flags = DOTA_DAMAGE_FLAG_NONE
 
 	-- Checking the mana of the target and calculating the damage
 	if(target:GetMana() >= manaBurn) then
@@ -26,6 +29,9 @@ function ManaBreak( keys )
 	end
 
 	ApplyDamage(damageTable)
+
+	target:EmitSound("Hero_Antimage.ManaBreak")
+	ParticleManager:CreateParticle("particles/generic_gameplay/generic_manaburn.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
 end
 
 
