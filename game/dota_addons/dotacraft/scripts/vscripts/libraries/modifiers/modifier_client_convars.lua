968bc56d361c2935423755f74f77fd30eb6eb5ca
MNoya <martinnoya@gmail.com>
2015-12-18 11:11:43 -0300

    Force autoattack convars

--- game/dota_addons/dotacraft/scripts/vscripts/libraries/modifiers/modifier_client_convars.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/libraries/modifiers/modifier_client_convars.lua
@@ -2,14 +2,20 @@ modifier_client_convars = class({})
 
 function modifier_client_convars:OnCreated( params )
     if not IsServer() then
-        self.original_value = Convars:GetBool("dota_player_add_summoned_to_selection")
+        self.original_autoattack_value = Convars:GetBool("dota_player_units_auto_attack")
+        self.original_addsummon_value = Convars:GetBool("dota_player_add_summoned_to_selection")
+        self.original_autospell_value = Convars:GetBool("dota_player_units_auto_attack_after_spell")
         Convars:SetBool("dota_player_add_summoned_to_selection", false)
+        Convars:SetBool("dota_player_units_auto_attack", true)
+        Convars:SetBool("dota_player_units_auto_attack_after_spell", true)
     end
 end
 
 function modifier_client_convars:OnDestroy( params )
     if not IsServer() then
-        Convars:SetBool("dota_player_add_summoned_to_selection", self.original_value)
+        Convars:SetBool("dota_player_add_summoned_to_selection", self.original_addsummon_value)
+        Convars:SetBool("dota_player_units_auto_attack", self.original_autoattack_value)
+        Convars:SetBool("dota_player_units_auto_attack_after_spell", true)
     end
 end
 
