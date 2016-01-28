62b510ac1818b9951f780d30ebb53dab8f4d911b
Infekma <de_atd@hotmail.com>
2016-01-28 16:50:40 +0000

    UI Refactored - Buttons
    - buttons now known as mechanics, scripts sub-directory added for
    mechanics which houses control_group and idle_workers (formerly
    buttons.js now seperated)

--- content/dota_addons/dotacraft/panorama/scripts/mechanics/control_group/control_group.js
+++ content/dota_addons/dotacraft/panorama/scripts/mechanics/control_group/control_group.js
@@ -1,64 +1,3 @@
-"use strict";
-
-var builderList;
-var idleCount = 0;
-var currentBuilder = 0;
-
-function OnIdleButtonPressed( data ) {
-	var iPlayerID = Players.GetLocalPlayer();
-	$.Msg("Player "+iPlayerID+" pressed iddle button, with "+idleCount+" idle builders");
-
-	if (currentBuilder == idleCount){
-		currentBuilder = 0;
-	};
-	currentBuilder++;
-
-	var nextBuilder = builderList[String(currentBuilder)];
-	if (nextBuilder === undefined)
-		currentBuilder = 1;
-
-	nextBuilder = builderList[String(currentBuilder)];
-	GameUI.SelectUnit(nextBuilder, false);
-	GameEvents.SendCustomGameEventToServer( "reposition_player_camera", { entIndex: nextBuilder });
-}
-
-function OnPlayerUpdateIdleBuilders( args ) {
-	var iPlayerID = Players.GetLocalPlayer();
-	//$.Msg("OnPlayerUpdateIdleBuilders")
-
-	builderList = args.idle_builder_entities;
-	idleCount = 0;
-
-	for (var key in builderList) {
-		var idleBuilderIndex = builderList[key];
-		//$.Msg("Idle Builder "+idleBuilderIndex)
-		idleCount++;
-	};
-
-	if (idleCount > 0){
-		$('#IdleNumber').text = idleCount;
-		$('#IdleButton').RemoveClass('Hidden');
-	}
-	else
-	{
-		$('#IdleNumber').text = "";
-		$('#IdleButton').AddClass('Hidden');
-	};
-};
-
-function OnPlayerStart( args ) {
-	var race = args.race;
-	idleCount = args.initial_builders;
-	$('#IdleNumber').text = idleCount;
-	$('#IdleButtonImage').SetImage( "s2r://panorama/images/custom_game/"+race+"/"+race+"_builder.png" );
-};
-
-var angle = 0;
-function RotateCamera() {
-	angle+= 180;
-	GameUI.SetCameraYaw( angle );
-};
- 
 function CreateControlGroup( index ){
 	var iPlayerID = Players.GetLocalPlayer();
     var selectedEntities = Players.GetSelectedEntities(iPlayerID);
@@ -112,14 +51,8 @@ function SortChildren( Container ){
 function isControlDown(){
 	return GameUI.IsControlDown();
 };
-(function () {
-	GameEvents.Subscribe( "player_show_ui", OnPlayerStart );
-	GameEvents.Subscribe( "rotate_camera", RotateCamera );
-	GameEvents.Subscribe( "player_update_idle_builders", OnPlayerUpdateIdleBuilders );
-	
-	// Idle Builders Key
-	Game.AddCommand( "+IdleBuilderSwap", OnIdleButtonPressed, "", 0 );
 
+(function () {
 	// Create Control Group Key
 	Game.AddCommand( "+SelectCreateControlGroup1", OnCreateControlGroupPressed, "", 0 );
 	Game.AddCommand( "+SelectCreateControlGroup2", OnCreateControlGroupPressed, "", 0 );
