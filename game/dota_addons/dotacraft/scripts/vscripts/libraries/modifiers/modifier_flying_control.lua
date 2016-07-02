ee4ad2281e01d4d651daf6c2bdae2a49047c7bb3
MNoya <martinnoya@gmail.com>
2016-07-02 19:28:05 -0300

    #287 Improved flying height control around trees

--- game/dota_addons/dotacraft/scripts/vscripts/libraries/modifiers/modifier_flying_control.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/libraries/modifiers/modifier_flying_control.lua
@@ -2,16 +2,23 @@ modifier_flying_control = class({})
 
 function modifier_flying_control:OnCreated()
     if IsServer() then
-        self.baseGround = GetGroundPosition(self:GetParent():GetAbsOrigin(), self:GetParent()).z + 128
+        self.baseGround = GetGroundPosition(self:GetParent():GetAbsOrigin(), self:GetParent()).z + 200
         self:StartIntervalThink(0.03)
+        self.tree_modifier_stacks = 0
     end
 end
 
 function modifier_flying_control:OnIntervalThink()
     local unit = self:GetParent()
     if unit:HasFlyMovementCapability() then
-        local z = math.max(0, self.baseGround - GetGroundPosition(unit:GetAbsOrigin(), unit).z)
-        self:SetStackCount(z)
+        local origin = unit:GetAbsOrigin()
+        local z = math.max(0, self.baseGround - GetGroundPosition(origin, unit).z)
+        if GridNav:IsNearbyTree(origin,64,true) then
+            self.tree_modifier_stacks = math.min(200, self.tree_modifier_stacks+10)
+        else
+            self.tree_modifier_stacks = math.max(0, self.tree_modifier_stacks-10)
+        end
+        self:SetStackCount(z+self.tree_modifier_stacks)
     else
         self:SetStackCount(0)
     end
