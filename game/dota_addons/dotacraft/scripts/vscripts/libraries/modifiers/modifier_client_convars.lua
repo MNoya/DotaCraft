4d82ffa4ac411776a4d290fa063fe7a95f62a3b2
MNoya <martinnoya@gmail.com>
2016-07-07 19:41:01 -0300

    Force unified orders off

--- game/dota_addons/dotacraft/scripts/vscripts/libraries/modifiers/modifier_client_convars.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/libraries/modifiers/modifier_client_convars.lua
@@ -5,8 +5,8 @@ function modifier_client_convars:OnCreated( params )
         SendToConsole("dota_player_add_summoned_to_selection 0")
         SendToConsole("dota_player_units_auto_attack_mode 2") --Always
         SendToConsole("dota_summoned_units_auto_attack_mode -1") --Same as hero
-        SendToConsole("dota_player_units_auto_attack_after_spell 1")
         SendToConsole("dota_force_right_click_attack 0")
+        SendToConsole("dota_player_multipler_orders 1")
     end
 end
 
