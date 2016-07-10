b10d3a5c4269185cea2cf08262986a594ecd223a
MNoya <martinnoya@gmail.com>
2016-07-09 22:45:31 -0300

    Updated flame strike, close #311
    https://gfycat.com/EmotionalAcclaimedFlyinglemur

--- game/dota_addons/dotacraft/scripts/vscripts/heroes/blood_mage/flame_strike.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/blood_mage/flame_strike.lua
@@ -1,6 +1,56 @@
+function FlameStrikeOrbThrow(event)
+    local caster = event.caster
+    local ability = event.ability
+    local point = event.target_points[1]
+
+    -- Remove first orb
+    local orbNumber
+    for i=1,3 do
+        if caster.orbs[i] then
+            ParticleManager:DestroyParticle(caster.orbs[i], true)
+            caster.orbs[i] = nil
+            orbNumber = i
+            break
+        end
+    end
+
+    -- Launch orb
+    local speed = 900
+    local orb = ParticleManager:CreateParticle("particles/units/heroes/hero_rubick/rubick_base_attack.vpcf", PATTACH_CUSTOMORIGIN, caster)
+    ParticleManager:SetParticleControl(orb, 0, caster:GetAttachmentOrigin(caster:ScriptLookupAttachment("attach_attack1")))
+    ParticleManager:SetParticleControl(orb, 1, point)
+    ParticleManager:SetParticleControl(orb, 2, Vector(speed, 0, 0))
+    ParticleManager:SetParticleControl(orb, 3, point)
+
+    local distanceToTarget = (caster:GetAbsOrigin() - point):Length2D()
+    Timers:CreateTimer(distanceToTarget/speed, function()
+        ParticleManager:DestroyParticle(orb, false)
+    end)
+
+    -- Restore orb
+    Timers:CreateTimer(1, function() 
+        if orbNumber then
+            caster.orbs[orbNumber] = ParticleManager:CreateParticle("particles/custom/human/blood_mage/exort_orb.vpcf", PATTACH_OVERHEAD_FOLLOW, caster)
+            ParticleManager:SetParticleControlEnt(caster.orbs[orbNumber], 1, caster, PATTACH_POINT_FOLLOW, "attach_orb"..orbNumber, caster:GetAbsOrigin(), false)
+        else
+            for i=1,3 do
+                caster.orbs[i] = ParticleManager:CreateParticle("particles/custom/human/blood_mage/exort_orb.vpcf", PATTACH_OVERHEAD_FOLLOW, caster)
+                ParticleManager:SetParticleControlEnt(caster.orbs[i], 1, caster, PATTACH_POINT_FOLLOW, "attach_orb"..i, caster:GetAbsOrigin(), false)
+            end
+        end
+    end)
+end
+
 function FlameStrikeAnimation(event)
     local caster = event.caster
-    StartAnimation(caster, {duration=1, activity=ACT_DOTA_CAST_SUN_STRIKE, rate=1, translate="divine_sorrow_sunstrike"})
+    local point = event.target_points[1]
+    StartAnimation(caster, {duration=1, activity=ACT_DOTA_CAST_TORNADO, rate=1})
+
+    local particle1 = ParticleManager:CreateParticle("particles/custom/human/blood_mage/invoker_sun_strike_team_immortal1.vpcf",PATTACH_CUSTOMORIGIN,caster)
+    ParticleManager:SetParticleControl(particle1,0,point)
+
+    local particle2 = ParticleManager:CreateParticle("particles/econ/items/shadow_fiend/sf_fire_arcana/sf_fire_arcana_requiemofsouls_line_ground.vpcf",PATTACH_CUSTOMORIGIN,caster)
+    ParticleManager:SetParticleControl(particle2,0,point)
 end
 
 function FlameStrikeDamage(event)
--- game/dota_addons/dotacraft/scripts/vscripts/heroes/blood_mage/orbs.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/blood_mage/orbs.lua
@@ -3,12 +3,9 @@ function AttachOrbs( event )
 	local origin = hero:GetAbsOrigin()
 	local particleName = "particles/custom/human/blood_mage/exort_orb.vpcf"
 
-	local orb1 = ParticleManager:CreateParticle(particleName, PATTACH_OVERHEAD_FOLLOW, hero)
-	ParticleManager:SetParticleControlEnt(orb1, 1, hero, PATTACH_POINT_FOLLOW, "attach_orb1", origin, false)
-
-	local orb2 = ParticleManager:CreateParticle(particleName, PATTACH_OVERHEAD_FOLLOW, hero)
-	ParticleManager:SetParticleControlEnt(orb2, 1, hero, PATTACH_POINT_FOLLOW, "attach_orb2", origin, false)
-
-	local orb3 = ParticleManager:CreateParticle(particleName, PATTACH_OVERHEAD_FOLLOW, hero)
-	ParticleManager:SetParticleControlEnt(orb3, 1, hero, PATTACH_POINT_FOLLOW, "attach_orb3", origin, false)
+    hero.orbs = {}
+    for i=1,3 do
+        hero.orbs[i] = ParticleManager:CreateParticle(particleName, PATTACH_OVERHEAD_FOLLOW, hero)
+        ParticleManager:SetParticleControlEnt(hero.orbs[i], 1, hero, PATTACH_POINT_FOLLOW, "attach_orb"..i, origin, false)
+    end
 end
