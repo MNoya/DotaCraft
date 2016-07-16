87a679163839d35b3db3fd1b8ef87663199159a8
MNoya <martinnoya@gmail.com>
2016-07-16 00:26:21 -0300

    Fix #319 and updated BM unit stats

--- game/dota_addons/dotacraft/scripts/vscripts/heroes/dread_lord/inferno.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/dread_lord/inferno.lua
@@ -1,4 +1,6 @@
 -- Changes the color of the summoned unit
 function RenderInferno(event)
-   event.target:SetRenderColor(128, 255, 0)
+    local infernal = event.target
+    infernal:SetRenderColor(128, 255, 0)
+    infernal:AddNewModifier(event.caster, event.ability, "modifier_summoned", {})
 end
