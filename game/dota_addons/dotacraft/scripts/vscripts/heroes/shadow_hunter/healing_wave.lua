140368f3f609aafd5332749c257d287ac977d4f8
MNoya <martinnoya@gmail.com>
2016-07-30 13:38:50 -0300

    Updated healing wave

--- game/dota_addons/dotacraft/scripts/vscripts/heroes/shadow_hunter/healing_wave.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/shadow_hunter/healing_wave.lua
@@ -36,7 +36,7 @@ function HealingWave( event )
 
             -- Hit the first unit with health deficit that hasn't been struck yet
             local bounce_target
-            for _,unit in pairs(units) do
+            for _,unit in pairs(allies) do
                 local entIndex = unit:GetEntityIndex()
                 if not targetsStruck[entIndex] and unit:GetHealthDeficit() > 0 then
                     bounce_target = unit
@@ -71,17 +71,18 @@ function CreateHealingWave(caster, start_position, target, healing, ability)
         target_position.z = target_position.z + target:GetBoundingMaxs().z
     end
 
-    local particle = ParticleManager:CreateParticle("particles/custom/dazzle_shadow_wave.vpcf", PATTACH_ABSORIGIN_FOLLOW, hero)
+    local particle = ParticleManager:CreateParticle("particles/custom/dazzle_shadow_wave.vpcf", PATTACH_CUSTOMORIGIN, nil)
     ParticleManager:SetParticleControl(particle, 0, start_position)
     ParticleManager:SetParticleControl(particle, 1, target_position)
 
-    local particle = ParticleManager:CreateParticle("particles/custom/dazzle_shadow_wave_copy.vpcf", PATTACH_ABSORIGIN_FOLLOW, hero)
+    local particle = ParticleManager:CreateParticle("particles/custom/dazzle_shadow_wave_copy.vpcf", PATTACH_CUSTOMORIGIN, nil)
     ParticleManager:SetParticleControl(particle, 0, start_position)
     ParticleManager:SetParticleControl(particle, 1, target_position)
 
     target:Heal(healing, target)
-    local heal = math.floor(math.min(healing, caster:GetHealthDeficit())+0.5)
+    local heal = math.floor(math.min(healing, target:GetHealthDeficit())+0.5)
     if heal > 0 then
         PopupHealing(target,heal)
     end
+    return target_position
 end
