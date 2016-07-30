0e563d279135c83fa0b35d3761a18e0e77dee430
MNoya <martinnoya@gmail.com>
2016-07-30 09:41:58 -0300

    Updated orb abilities and items

--- game/dota_addons/dotacraft/scripts/vscripts/heroes/firelord/incinerate.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/firelord/incinerate.lua
@@ -1,10 +1,22 @@
+function OrbCheck(event)
+    local target = event.target
+    local caster = event.caster
+
+    if target:IsMechanical() or IsCustomBuilding(target) then
+        caster:RemoveModifierByName("modifier_incinerate_orb")
+    else
+        if not caster:HasModifier("modifier_incinerate_orb") then
+            local ability = event.ability
+            ability:ApplyDataDrivenModifier(caster,caster,"modifier_incinerate_orb",{})
+        end
+    end
+end
+
 --[[
 	Author: Noya, adapted from SpellLibrary ursa_fury_swipes
-	Date: 05.02.2015.
 	Increase stack after each hit
 ]]
 function IncinerateAttack( event )
-	-- Variables
 	local caster = event.caster
 	local target = event.target
 	local ability = event.ability
@@ -18,21 +30,28 @@ function IncinerateAttack( event )
 	if target:HasModifier( modifierName ) then
 		local current_stack = target:GetModifierStackCount( modifierName, ability )
 		
-		-- Deal damage
-		local damage_table = {
-			victim = target,
-			attacker = caster,
-			damage = damage_per_stack * current_stack,
-			damage_type = damageType,
-			ability = ability, 
-		}
-		ApplyDamage( damage_table )
+		ApplyDamage({victim = target, attacker = caster, damage = damage_per_stack * current_stack, damage_type = damageType, ability = ability})
 		
-		ability:ApplyDataDrivenModifier( caster, target, modifierName, { Duration = duration } )
+		ability:ApplyDataDrivenModifier( caster, target, modifierName, { duration = duration } )
 		target:SetModifierStackCount( modifierName, ability, current_stack + 1 )
 	else
-		ability:ApplyDataDrivenModifier( caster, target, modifierName, { Duration = duration } )
+		ability:ApplyDataDrivenModifier( caster, target, modifierName, { duration = duration } )
 		target:SetModifierStackCount( modifierName, ability, 1 )
 	end
+end
 
+function IncinerateDeath(event)
+	local caster = event.caster
+	local target = event.unit
+	local ability = event.ability
+	local radius = ability:GetLevelSpecialValueFor("incineration_radius",ability:GetLevel()-1)
+	local damage = ability:GetLevelSpecialValueFor("incineration_damage",ability:GetLevel()-1)
+	
+	local particle = ParticleManager:CreateParticle("particles/custom/ogre_magi_fireblast.vpcf", PATTACH_CUSTOMORIGIN, nil)
+	ParticleManager:SetParticleControl(particle, 0, target:GetAbsOrigin())
+
+	local enemies = FindUnitsInRadius(caster:GetTeamNumber(), target:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, 0, FIND_ANY_ORDER, false)
+	for _,v in pairs(enemies) do
+		ApplyDamage({victim = v, attacker = caster, damage = damage, damage_type = DAMAGE_TYPE_MAGICAL, ability = ability})
+	end
 end
--- game/dota_addons/dotacraft/scripts/vscripts/items/orbs.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/items/orbs.lua
@@ -1,39 +1,67 @@
 function EquipOrb( event )
-	print("EquipOrb")
-
-	local caster = event.caster
-	if caster:IsHero() then
-		if not caster.original_attack then
-			caster.original_attack = caster:GetAttacksEnabled()
-		end
-		caster:SetAttacksEnabled("ground, air")
-	end
+    local caster = event.caster
+    if caster:IsHero() then
+        if not caster.original_attack then
+            caster.original_attack = caster:GetAttacksEnabled()
+        end
+        caster:SetAttacksEnabled("ground, air")
+    end
 end
 
 function UnequipOrb( event )
-	print("UnequipOrb")
+    local caster = event.caster
+    if caster:IsHero() then
+        caster:SetAttacksEnabled(caster.original_attack)
+        if caster.original_attack_type then
+            caster:SetAttackCapability(caster.original_attack_type)
+        end
+    end
+end
 
-	local caster = event.caster
-	if caster:IsHero() then
-		print("Set Attacks Enabled to ",caster.original_attack)
-		caster:SetAttacksEnabled(caster.original_attack)
+function Splash(event)
+    local attacker = event.caster
+    local target = event.target
+    local ability = event.ability
+    local radius = ability:GetSpecialValueFor("full_damage_radius")
+    local full_damage = attacker:GetAttackDamage()
+    
+    local splash_targets = FindUnitsInRadius(attacker:GetTeamNumber(), target:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false)
+    for _,unit in pairs(splash_targets) do
+        if unit ~= target and not unit:HasFlyMovementCapability() and not IsCustomBuilding(unit) then
+            ApplyDamage({victim = unit, attacker = attacker, damage = full_damage, ability = ability, damage_type = DAMAGE_TYPE_PHYSICAL})
+        end
+    end
+end
 
-		if caster.original_attack_type then
-			caster:SetAttackCapability(caster.original_attack_type)
-		end
-	end
+function Purge(event)
+    local caster = event.caster
+    local target = event.target
+    local ability = event.ability
+    local duration = ability:GetSpecialValueFor('duration')
+    local bSummoned = target:IsSummoned()
+
+    target:Purge(true, false, false, false, true)
+    target:RemoveModifierByName("modifier_brewmaster_storm_cyclone")
+    ParticleManager:CreateParticle('particles/generic_gameplay/generic_purge.vpcf', PATTACH_ABSORIGIN_FOLLOW, target)
+    target:EmitSound("DOTA_Item.DiffusalBlade.Target")
+
+    ability:ApplyDataDrivenModifier(caster, target, 'modifier_purge', {duration = duration}) 
+
+    if bSummoned then
+        ApplyDamage({victim = target, attacker = caster, damage = ability:GetSpecialValueFor('damage_to_summons'), damage_type = DAMAGE_TYPE_PURE, ability = ability})
+    end
 end
 
 function OrbAirCheck( event )
-	local attacker = event.attacker
-	local target = event.target
-	local target_type = GetMovementCapability(target)
-	if not attacker.original_attack_type then
-		attacker.original_attack_type = attacker:GetAttackCapability()
-	end
-	if target_type == "air" then
-		attacker:SetAttackCapability(DOTA_UNIT_CAP_RANGED_ATTACK)
-	else
-		attacker:SetAttackCapability(attacker.original_attack_type)
-	end
+    local attacker = event.attacker
+    local target = event.target
+    local target_type = GetMovementCapability(target)
+    if not attacker.original_attack_type then
+        attacker.original_attack_type = attacker:GetAttackCapability()
+    end
+    if target_type == "air" then
+        attacker:SetAttackCapability(DOTA_UNIT_CAP_RANGED_ATTACK)
+    else
+        attacker:SetAttackCapability(attacker.original_attack_type)
+    end
 end
