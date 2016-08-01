b6e736b030741653d5eb02424869060a6215d7e3
MNoya <martinnoya@gmail.com>
2016-08-01 09:35:04 -0300

    Updated all Pit Lord's abilities
    (giff model)

--- game/dota_addons/dotacraft/scripts/vscripts/heroes/pit_lord/doom.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/pit_lord/doom.lua
@@ -1,4 +1,15 @@
--- Stops Doom looping sound
-function DoomStopSound( event )
-    StopSoundEvent("Hero_DoomBringer.Doom", event.unit)
+function DoomStart(event)
+    event.target:SetNoCorpse()
+end
+
+function DoomEnd( event )
+    local caster = event.caster
+    local target = event.unit
+    local duration = event.ability:GetSpecialValueFor("doom_guard_duration")
+
+    local doom_guard = caster:CreateSummon("undead_doom_guard", target:GetAbsOrigin(), duration)
+    ParticleManager:CreateParticle("particles/units/heroes/hero_doom_bringer/doom_bringer_lvl_death.vpcf", PATTACH_ABSORIGIN_FOLLOW, doom_guard)
+    ParticleManager:CreateParticle("particles/econ/items/doom/doom_f2p_death_effect/doom_bringer_f2p_death.vpcf", PATTACH_ABSORIGIN_FOLLOW, doom_guard)
+    doom_guard:EmitSound("Hero_DoomBringer.LvlDeath")   
+    StopSoundEvent("Hero_DoomBringer.Doom", target)
 end
