4626e00f077a47959dafd0e81a8527ff47a26fa9
MNoya <martinnoya@gmail.com>
2016-08-01 12:11:25 -0300

    Updated Tranquility and BB Voodoo auras to lua

--- game/dota_addons/dotacraft/scripts/vscripts/heroes/keeper/tranquility.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/keeper/tranquility.lua
@@ -1,12 +1,106 @@
-function TranquilityStart( event )
-	local caster = event.caster
-	
-	StartAnimation(caster, {duration=2, activity=ACT_DOTA_CAST_ABILITY_2, rate=0.8, translate="torment"})
+keeper_tranquility = class({})
 
+LinkLuaModifier("modifier_tranquility_aura", "heroes/keeper/tranquility", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_tranquility_aura_effect", "heroes/keeper/tranquility", LUA_MODIFIER_MOTION_NONE)
+
+function keeper_tranquility:OnSpellStart()
+    local duration = self:GetSpecialValueFor("duration")
+    local caster = self:GetCaster()
+    caster:AddNewModifier(caster,self,"modifier_tranquility_aura",{duration=duration})
+    caster:EmitSound("leshrac_lesh_respawn_09")
+    Timers:CreateTimer(1, function()
+        if self:IsChanneling() then
+            StartAnimation(caster, {duration=2, activity=ACT_DOTA_CAST_ABILITY_2, rate=0.8, translate="torment"})
+            return 2
+        end
+    end)
 end
 
-function TranquilityThink( event )
-	local caster = event.caster
-	
-	StartAnimation(caster, {duration=2, activity=ACT_DOTA_CAST_ABILITY_2, rate=0.8, translate="torment"})
+function keeper_tranquility:OnChannelFinish(bInterrupted)
+    local caster = self:GetCaster()
+    caster:RemoveModifierByName("modifier_tranquility_aura")
+    EndAnimation(caster)
 end
+
+function keeper_tranquility:GetCastAnimation()
+    return ACT_DOTA_CAST_ABILITY_2
+end
+
+--------------------------------------------------------------------------------
+
+modifier_tranquility_aura = class({})
+
+function modifier_tranquility_aura:OnCreated()
+    if IsServer() then
+        local caster = self:GetCaster()
+        local particle = ParticleManager:CreateParticle("particles/custom/nightelf/tranquility.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
+        ParticleManager:SetParticleControl(particle,1,Vector(self:GetAbility():GetSpecialValueFor("radius"), 0, 0))
+        ParticleManager:SetParticleControlEnt(particle, 2, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
+        self:AddParticle(particle, false, false, 1, false, false)
+    end
+end
+
+function modifier_tranquility_aura:IsAura() return true end
+function modifier_tranquility_aura:IsHidden() return false end
+function modifier_tranquility_aura:IsPurgable() return false end
+
+function modifier_tranquility_aura:GetAuraRadius()
+    return self:GetAbility():GetSpecialValueFor("radius")
+end
+
+function modifier_tranquility_aura:GetModifierAura()
+    return "modifier_tranquility_aura_effect"
+end
+
+function modifier_tranquility_aura:GetEffectName()
+    return "particles/items2_fx/tranquil_boots_healing.vpcf"
+end
+
+function modifier_tranquility_aura:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
+   
+function modifier_tranquility_aura:GetAuraSearchTeam()
+    return DOTA_UNIT_TARGET_TEAM_FRIENDLY
+end
+
+function modifier_tranquility_aura:GetAuraEntityReject(target)
+    return IsCustomBuilding(target)
+end
+
+function modifier_tranquility_aura:GetAuraSearchType()
+    return DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC
+end
+
+function modifier_tranquility_aura:GetAuraDuration()
+    return 0.5
+end
+
+--------------------------------------------------------------------------------
+
+modifier_tranquility_aura_effect = class({})
+
+function modifier_tranquility_aura_effect:OnCreated()
+    if IsServer() then
+        self:StartIntervalThink(1)
+    end
+end
+
+function modifier_tranquility_aura_effect:OnIntervalThink()
+    local target = self:GetParent()
+    ParticleManager:CreateParticle("particles/neutral_fx/troll_heal.vpcf",PATTACH_ABSORIGIN_FOLLOW,target)
+end
+
+function modifier_tranquility_aura_effect:DeclareFunctions()
+    return { MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT }
+end
+
+function modifier_tranquility_aura_effect:GetModifierConstantHealthRegen()
+    return self:GetAbility():GetSpecialValueFor("heal_per_second")
+end
+
+function modifier_tranquility_aura_effect:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
+
+function modifier_tranquility_aura_effect:IsPurgable() return false end
--- game/dota_addons/dotacraft/scripts/vscripts/heroes/shadow_hunter/big_bad_voodoo.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/shadow_hunter/big_bad_voodoo.lua
@@ -1,10 +1,99 @@
---[[
-	Author: Noya
-	Date: 28.01.2015
-	Stops the looping sound event
-]]
-function MaledictStop( event )
-	local caster = event.caster
-	
-	caster:StopSound("Hero_WitchDoctor.Maledict_Loop")
+shadow_hunter_big_bad_voodoo = class({})
+
+LinkLuaModifier("modifier_big_bad_voodoo", "heroes/shadow_hunter/big_bad_voodoo", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_big_bad_voodoo_invulnerability", "heroes/shadow_hunter/big_bad_voodoo", LUA_MODIFIER_MOTION_NONE)
+
+function shadow_hunter_big_bad_voodoo:OnSpellStart()
+    local duration = self:GetSpecialValueFor("duration")
+    local caster = self:GetCaster()
+    caster:AddNewModifier(caster,self,"modifier_big_bad_voodoo",{duration=duration})
+    caster:EmitSound("shadowshaman_shad_ability_shackle_08")
+    Timers:CreateTimer(1, function()
+        if self:IsChanneling() then
+            caster:StartGesture(ACT_DOTA_CAST_ABILITY_4)
+            return 1
+        end
+    end)
 end
+
+function shadow_hunter_big_bad_voodoo:OnChannelFinish(bInterrupted)
+    local caster = self:GetCaster()
+    caster:RemoveModifierByName("modifier_big_bad_voodoo")
+    caster:StopSound("Hero_WitchDoctor.Maledict_Loop")
+    caster:RemoveGesture(ACT_DOTA_CAST_ABILITY_4)
+end
+
+function shadow_hunter_big_bad_voodoo:GetCastAnimation()
+    return ACT_DOTA_CAST_ABILITY_4
+end
+
+--------------------------------------------------------------------------------
+
+modifier_big_bad_voodoo = class({})
+
+function modifier_big_bad_voodoo:OnCreated()
+    local target = self:GetParent()
+    local particle = ParticleManager:CreateParticle("particles/custom/witchdoctor_voodoo_restoration_aura.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+    self:AddParticle(particle, false, false, 1, false, false)
+ 
+    local particle = ParticleManager:CreateParticle("particles/custom/warlock_shadow_word_buff_copy.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+    self:AddParticle(particle, false, false, 1, false, false)
+
+    local particle = ParticleManager:CreateParticle("particles/custom/witchdoctor_voodoo_restoration.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+    ParticleManager:SetParticleControl(particle,1,Vector(self:GetAbility():GetSpecialValueFor("radius"), 0, 0))
+    self:AddParticle(particle, false, false, 1, false, false)
+end
+
+function modifier_big_bad_voodoo:IsAura() return true end
+function modifier_big_bad_voodoo:IsHidden() return true end
+function modifier_big_bad_voodoo:IsPurgable() return false end
+
+function modifier_big_bad_voodoo:GetAuraRadius()
+    return self:GetAbility():GetSpecialValueFor("radius")
+end
+
+function modifier_big_bad_voodoo:GetModifierAura()
+    return "modifier_big_bad_voodoo_invulnerability"
+end
+
+function modifier_big_bad_voodoo:GetEffectName()
+    return "particles/units/heroes/hero_warlock/warlock_shadow_word_debuff.vpcf"
+end
+
+function modifier_big_bad_voodoo:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
+   
+function modifier_big_bad_voodoo:GetAuraSearchTeam()
+    return DOTA_UNIT_TARGET_TEAM_FRIENDLY
+end
+
+function modifier_big_bad_voodoo:GetAuraEntityReject(target)
+    return IsCustomBuilding(target) or target == self:GetCaster()
+end
+
+function modifier_big_bad_voodoo:GetAuraSearchType()
+    return DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC
+end
+
+function modifier_big_bad_voodoo:GetAuraDuration()
+    return 0.5
+end
+
+--------------------------------------------------------------------------------
+
+modifier_big_bad_voodoo_invulnerability = class({})
+
+function modifier_big_bad_voodoo_invulnerability:CheckState()
+    return { [MODIFIER_STATE_INVULNERABLE] = true }
+end
+
+function modifier_big_bad_voodoo_invulnerability:GetEffectName()
+    return "particles/custom/warlock_shadow_word_buff_c.vpcf"    
+end
+
+function modifier_big_bad_voodoo_invulnerability:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
+
+function modifier_big_bad_voodoo_invulnerability:IsPurgable() return false end
