b7b5804eea8bf0968cec8be0643516afaaad7d12
MNoya <martinnoya@gmail.com>
2016-08-07 16:06:38 -0300

    Updated Spirit Walker abilities

--- game/dota_addons/dotacraft/scripts/vscripts/units/orc/spiritwalker.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/units/orc/spiritwalker.lua
@@ -1,26 +1,46 @@
-function EtherStart( event )
+function EtherealStart(event)
     local caster = event.caster
     local ability = event.ability
-    caster:EmitSound('Hero_Pugna.Decrepify')
-    ability:ApplyDataDrivenModifier(caster, caster, 'modifier_ethereal', {})
-    local cooldown = ability:GetCooldown(0)
-    ability:StartCooldown(cooldown)
-    local another = caster:FindAbilityByName('orc_corporeal_form')
-    another:StartCooldown(cooldown)
-    ability:SetHidden(true)
-    another:SetHidden(false)
+    ability:EndCooldown()
+    StartAnimation(caster, {duration=0.7, activity=ACT_DOTA_CAST_ABILITY_4, rate=1.5})
+    caster:EmitSound("Hero_Pugna.Decrepify")
+    ability:ApplyDataDrivenModifier(caster,caster,"modifier_ethereal_form_transform",{})
+    ability:ToggleOn()
 end
 
-function EtherEnd( event )
+function EtherealForm(event)
     local caster = event.caster
     local ability = event.ability
-    caster:RemoveModifierByNameAndCaster('modifier_ethereal', caster)
-    local another = caster:FindAbilityByName('orc_ethereal_form')
-    ability:SetHidden(true)
-    another:SetHidden(false)
+    ability:ApplyDataDrivenModifier(caster, caster, "modifier_ethereal", {})
+    
+    local orc_corporeal_form = caster:FindAbilityByName("orc_corporeal_form")
+    orc_corporeal_form:StartCooldown(orc_corporeal_form:GetCooldown(1))
+    caster:SwapAbilities("orc_corporeal_form","orc_ethereal_form",true,false)
+    orc_corporeal_form:ToggleOff()
 end
 
-function Disenchant( event )
+function CorporealStart(event)
+    local caster = event.caster
+    local ability = event.ability
+    StartAnimation(caster, {duration=0.7, activity=ACT_DOTA_CAST_ABILITY_4, rate=1.5})
+    ability:ApplyDataDrivenModifier(caster,caster,"modifier_corporeal_form_transform",{})
+    caster:EmitSound("Hero_Spirit_Breaker.NetherStrike.Begin")
+    ability:EndCooldown()
+    ability:ToggleOn()
+end
+
+function CorporealForm(event)
+    local caster = event.caster
+    local ability = event.ability
+    caster:RemoveModifierByNameAndCaster("modifier_ethereal", caster)
+    caster:SwapAbilities("orc_corporeal_form","orc_ethereal_form",false,true)
+    local orc_ethereal_form = caster:FindAbilityByName("orc_ethereal_form")
+    orc_ethereal_form:ToggleOff()
+end
+
+----------------------------------------------------------------------
+
+function Disenchant(event)
     local caster = event.caster
     local ability = event.ability
     local point = event.target_points[1]
@@ -48,111 +68,134 @@ function Disenchant( event )
     Blight:Dispel(point)
 end
 
-function SpiritLinkStart( event )
+----------------------------------------------------------------------
+-- Damage is distributed across all linked units on the team
+if not _G.SpiritLinkTable then
+    _G.SpiritLinkTable = {}
+end
+
+function SpiritLinkStart(event)
     local caster = event.caster
     local target = event.target
     local ability = event.ability
     local radius = ability:GetSpecialValueFor('radius')
+    local teamID = caster:GetTeamNumber()
+    SpiritLinkTable[teamID] = SpiritLinkTable[teamID] or {}
 
-    local units = 0
-    local max = ability:GetSpecialValueFor('max_unit')
     local allies = FindUnitsInRadius(caster:GetTeamNumber(), target:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_BASIC + DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NOT_MAGIC_IMMUNE_ALLIES, FIND_CLOSEST, false)
-    if caster.linked == nil then
-        caster.linked = {}
-    end
+    local max = ability:GetSpecialValueFor('max_unit')
+    
+    -- The main target is always linked
     ability:ApplyDataDrivenModifier(caster, target, 'modifier_spirit_link', {})
-    local anyunit = false
-    local linked = {}
-    while units < max do
-        for k,ally in pairs(allies) do
-            if units < max and (not ally:FindModifierByName('modifier_spirit_link') or anyunit or ally ~= caster) then
+    local units = 1
+    local linked = {} -- Chosen units
+    table.insert(linked, target)
+
+    -- First prioritize those that don't have the modifier
+    for _,ally in pairs(allies) do
+        if units < max then
+            if not ally:HasModifier('modifier_spirit_link') then
                 ParticleManager:CreateParticle("particles/custom/orc/spirit_link_cast.vpcf", PATTACH_ABSORIGIN, ally)
                 ability:ApplyDataDrivenModifier(caster, ally, 'modifier_spirit_link', {})
-                units = units + 1
                 table.insert(linked, ally)
+                units = units + 1
             end
+        else
+            break
         end
-        anyunit = true
     end
 
-    for _,unit in pairs(linked) do
-        for _,otherUnit in pairs(linked) do
-            if otherUnit ~= unit then
-                local particle = ParticleManager:CreateParticle("particles/custom/dazzle_shadow_wave_b.vpcf", PATTACH_CUSTOMORIGIN, nil)
-                ParticleManager:SetParticleControl(particle, 0, unit:GetAbsOrigin())
-                ParticleManager:SetParticleControl(particle, 1, otherUnit:GetAbsOrigin())
+    -- Then if we havent reached the max, refresh others
+    if units < max then
+        for _,ally in pairs(allies) do
+            if units < max then
+                if ally:HasModifier('modifier_spirit_link') and ally ~= target then
+                    ParticleManager:CreateParticle("particles/custom/orc/spirit_link_cast.vpcf", PATTACH_ABSORIGIN, ally)
+                    ability:ApplyDataDrivenModifier(caster, ally, 'modifier_spirit_link', {})
+                    table.insert(linked, ally)
+                    units = units + 1
+                end
+            else
+                break
             end
         end
+    end
+
+    -- Make a chain between the linked units
+    local origin = target
+    for _,unit in pairs(linked) do
+        local particle = ParticleManager:CreateParticle("particles/custom/dazzle_shadow_wave_b.vpcf", PATTACH_CUSTOMORIGIN, nil)
+        ParticleManager:SetParticleControl(particle, 0, origin:GetAbsOrigin())
+        ParticleManager:SetParticleControl(particle, 1, unit:GetAbsOrigin())
+        origin = unit
     end
 end
 
-function RemoveLinkedUnit( event )
+
+-- Modifier created
+function AddLinkedUnit(event)
+    local unit = event.target
+    unit.entIndex = unit:GetEntityIndex()
+    SpiritLinkTable[unit:GetTeamNumber()][unit.entIndex] = unit
+end
+
+-- Modifier destroyed
+function RemoveLinkedUnit(event)
     local unit = event.target
     if IsValidEntity(unit) then
-        -- print('=======================')
-        local i = getIndex(event.caster.linked, unit:GetEntityIndex())
-        if i ~= -1 then
-            table.remove(event.caster.linked, i)
-            -- print('Unit removed from table')
-        else
-            -- print("Invalid index")
-        end
-        -- print('=======================')
+        SpiritLinkTable[unit:GetTeamNumber()][unit.entIndex] = nil        
     end
 end
 
-function AddLinkedUnit( event )
-    local unit = event.target
-    table.insert(event.caster.linked, unit:GetEntityIndex())
-end
-
-function LinkDamage( event )
+-- Damage taken, post mitigation
+function LinkDamage(event)
     local attacker = event.attacker
+    local caster = event.caster
     local target = event.unit
     local damage = event.Damage
     local ability = event.ability
-    local factor = ability:GetSpecialValueFor('distribution_factor') 
+    local factor = ability:GetSpecialValueFor('distribution_factor')
 
-    if IsValidAlive(target) then
-        target:Heal(damage * factor, attacker)
+    local linked = SpiritLinkTable[target:GetTeamNumber()]
+    if not linked then
+        print("Error, no Spirit Link Table for team "..target:GetTeamNumber())
+        return
     end
 
-    local j = TableFindKey(event.caster.linked, target:GetEntityIndex())
-    local k = TableCount(event.caster.linked)
+    local count = TableCount(linked)
+    if count == 1 then return end -- Take full damage
 
-    -- DeepPrintTable(event.caster.linked)
-    if not j then j = -1 end
-    -- print('=======================')
-    -- print('Damage on main target: ' .. damage * factor)
-    -- print('Index of main target: ' .. j)
-    -- print('Table general count: ' .. k)
-    -- print('=======================')
-    if k-1 > 0 then
-        local dist_damage = (damage * factor) / (k-1)
-        for i=1,k do
-            if i ~= j then
-                if event.caster.linked[i] then
-                    local linked = EntIndexToHScript(event.caster.linked[i])
-                    if IsValidAlive(linked) then
-                        local new_health = linked:GetHealth() - dist_damage
-                        if new_health < 1 then
-                            new_health = 1
-                            linked:RemoveModifierByName('modifier_spirit_link')
-                        end
-                        linked:SetHealth(new_health)
-                        -- print('=======================')
-                        -- print('Damage on linked unit: ' .. dist_damage)
-                        -- print('Index of linked unit: ' .. i)
-                        -- print('=======================')
-                    end
-                end
+    -- Revert the damage taken
+    local distribute_damage = damage * factor
+    local damage_prevented = damage - distribute_damage
+    local currentHP = target:GetHealth()
+    if currentHP > 0 then
+        target:SetHealth(currentHP+damage_prevented)
+    else -- Last hit wont distribute damage
+        target:RemoveModifierByName("modifier_spirit_link")
+        return
+    end
+
+    -- Split the damage between all other units including the damaged unit
+    distribute_damage = distribute_damage / count
+    for _,unit in pairs(linked) do
+        if IsValidAlive(unit) then
+            local new_health = unit:GetHealth() - distribute_damage
+            if new_health <= 1 then
+                new_health = 1
+                unit:RemoveModifierByName("modifier_spirit_link")
             end
+            unit:SetHealth(new_health)
+        else
+            linked[unit.entIndex] = nil
         end
     end
 end
 
+----------------------------------------------------------------------
+
 -- Denies casting if no tauren corpses near or not enough food to support revival, with a message
-function AncestralSpiritPrecast( event )
+function AncestralSpiritPrecast(event)
     local ability = event.ability
     local caster = event.caster
     local radius = ability:GetCastRange()
@@ -177,7 +220,7 @@ function AncestralSpiritPrecast( event )
 end
 
 -- Resurrects units near the caster, using the corpse mechanic.
-function AncestralSpirit( event )
+function AncestralSpirit(event)
     local caster = event.caster
     local ability = event.ability
     local health_factor = ability:GetSpecialValueFor('life_restored') * 0.01
