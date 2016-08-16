d7e8a6a5242bcebc9903895a8930a1139f695e3b
MNoya <martinnoya@gmail.com>
2016-08-16 00:16:14 -0300

    Updated disease cloud visuals

---
+++ game/dota_addons/dotacraft/scripts/vscripts/units/undead/abomination.lua
@@ -0,0 +1,87 @@
+
+undead_disease_cloud = class({})
+
+LinkLuaModifier("modifier_disease_cloud_aura", "units/undead/abomination.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_disease_cloud_debuff", "units/undead/abomination.lua", LUA_MODIFIER_MOTION_NONE)
+
+function undead_disease_cloud:GetIntrinsicModifierName()
+    return "modifier_disease_cloud_aura"
+end
+
+--------------------------------------------------------------------------------
+
+modifier_disease_cloud_aura = class({})
+
+function modifier_disease_cloud_aura:OnCreated()
+    if IsServer() then
+        local particle = ParticleManager:CreateParticle("particles/custom/undead/disease_cloud.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
+        ParticleManager:SetParticleControl(particle, 1, Vector(200,0,0))
+        self:AddParticle(particle, false, false, 1, false, false)
+    end
+end
+
+function modifier_disease_cloud_aura:IsAura()
+    return true
+end
+
+function modifier_disease_cloud_aura:IsHidden()
+    return true
+end
+
+function modifier_disease_cloud_aura:IsPurgable()
+    return false
+end
+
+function modifier_disease_cloud_aura:GetAuraRadius()
+    return self:GetAbility():GetSpecialValueFor("radius")
+end
+
+function modifier_disease_cloud_aura:GetModifierAura()
+    return "modifier_disease_cloud_debuff"
+end
+   
+function modifier_disease_cloud_aura:GetAuraSearchTeam()
+    return DOTA_UNIT_TARGET_TEAM_ENEMY
+end
+
+function modifier_disease_cloud_aura:GetAuraEntityReject(target)
+    return IsCustomBuilding(target) or target:IsWard() or target:IsMechanical()
+end
+
+function modifier_disease_cloud_aura:GetAuraSearchType()
+    return DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC
+end
+
+function modifier_disease_cloud_aura:GetAuraDuration()
+    return 128
+end
+
+--------------------------------------------------------------------------------
+
+modifier_disease_cloud_debuff = class({})
+
+function modifier_disease_cloud_debuff:IsPurgable()
+    return false
+end
+
+function modifier_disease_cloud_debuff:IsDebuff()
+    return true
+end
+
+function modifier_disease_cloud_debuff:OnCreated()
+    if IsServer() then
+        self:StartIntervalThink(1)
+    end
+end
+
+function modifier_disease_cloud_debuff:GetEffectName()
+    return "particles/custom/undead/disease_debuff.vpcf"
+end
+
+function modifier_disease_cloud_debuff:GetEffectAttachType()
+    return PATTACH_ABSORIGIN_FOLLOW
+end
+
+function modifier_disease_cloud_debuff:OnIntervalThink()
+    ApplyDamage({victim = self:GetParent(), attacker = self:GetCaster(), damage = 1, damage_type = DAMAGE_TYPE_MAGICAL})
+end
