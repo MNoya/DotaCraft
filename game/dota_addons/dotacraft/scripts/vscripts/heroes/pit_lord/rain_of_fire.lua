39b9f4d8571400342a9dc7964f59d5e04e12e92a
MNoya <martinnoya@gmail.com>
2016-08-23 16:41:39 -0300

    Pit Lord

--- game/dota_addons/dotacraft/scripts/vscripts/heroes/pit_lord/rain_of_fire.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/heroes/pit_lord/rain_of_fire.lua
@@ -14,13 +14,15 @@ end
 function ApplyAnimation(event)
     local ability = event.ability
     local caster = event.caster
-    local start_time = ability:GetChannelStartTime()
-    local time_channeled = GameRules:GetGameTime() - start_time
-    local max_channel_time = ability:GetChannelTime()
 
-    if ability:IsChanneling() and (time_channeled < max_channel_time - 1) then
-        caster:StartGesture(ACT_DOTA_SPAWN)
-    end
+    Timers:CreateTimer(0.5, function()
+        local start_time = ability:GetChannelStartTime()
+        local time_channeled = GameRules:GetGameTime() - start_time
+        local max_channel_time = ability:GetChannelTime()
+        if ability:IsChanneling() and (time_channeled < max_channel_time-1) then
+            caster:StartGesture(ACT_DOTA_CAST_ABILITY_1)
+        end
+    end)
 end
 
 function RainOfFireWave(event)
@@ -30,17 +32,23 @@ function RainOfFireWave(event)
     local damage = ability:GetLevelSpecialValueFor("wave_damage",ability:GetLevel()-1)
     local max_wave_damage = ability:GetLevelSpecialValueFor("max_wave_damage",ability:GetLevel()-1)
     local target_position = event.target:GetAbsOrigin() --event.target_points[1]
-    local particleName = "particles/units/heroes/hero_crystalmaiden/maiden_freezing_field_explosion.vpcf"
+    local particleName = "particles/custom/neutral/firestorm_wave.vpcf"
     local distance = 100
 
-    local particle1 = ParticleManager:CreateParticle("particles/units/heroes/hero_phoenix/phoenix_fire_spirit_ground.vpcf", PATTACH_CUSTOMORIGIN, caster)
+    local firestorm = ParticleManager:CreateParticle(particleName, PATTACH_CUSTOMORIGIN, nil)
+    ParticleManager:SetParticleControl(firestorm, 0, target_position)
+    ParticleManager:SetParticleControl(firestorm, 4, Vector(radius, 1, 1))
+
+    --[[local particle1 = ParticleManager:CreateParticle("particles/units/heroes/hero_phoenix/phoenix_fire_spirit_ground.vpcf", PATTACH_CUSTOMORIGIN, caster)
     ParticleManager:SetParticleControl( particle1, 0, target_position )
 
     local particle2 = ParticleManager:CreateParticle("particles/units/heroes/hero_warlock/warlock_rain_of_chaos_explosion.vpcf", PATTACH_CUSTOMORIGIN, caster)
     ParticleManager:SetParticleControl( particle2, 0, target_position )
 
     local particle3 = ParticleManager:CreateParticle("particles/units/heroes/hero_invoker/invoker_sun_strike.vpcf", PATTACH_CUSTOMORIGIN, caster)
-    ParticleManager:SetParticleControl( particle3, 0, target_position )
+    ParticleManager:SetParticleControl( particle3, 0, target_position )]]
+
+    caster:EmitSound("Hero_AbyssalUnderlord.Firestorm.Start")
 
     ability:ApplyDamageUnitsMax(damage, FindAllUnitsInRadius(caster, radius, target_position), max_wave_damage)
 end
