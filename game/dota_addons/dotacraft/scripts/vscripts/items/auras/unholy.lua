df34e6f8a888688ec91275d1d348ad1741ab52d6
MNoya <martinnoya@gmail.com>
2016-09-01 08:46:05 -0300

    Close #301

--- game/dota_addons/dotacraft/scripts/vscripts/items/auras/unholy.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/items/auras/unholy.lua
@@ -5,9 +5,5 @@ LinkLuaModifier("modifier_unholy_aura", "heroes/death_knight/unholy_aura", LUA_M
 LinkLuaModifier("modifier_unholy_aura_buff", "heroes/death_knight/unholy_aura", LUA_MODIFIER_MOTION_NONE)
 
 function item_legion_doom_horn:GetIntrinsicModifierName()
-    if self:GetCaster():HasModifier("modifier_unholy_aura") then
-        return ""
-    else
-        return "modifier_unholy_aura"
-    end
+    return "modifier_unholy_aura"
 end
--- game/dota_addons/dotacraft/scripts/vscripts/items/auras/vampiric.lua
+++ game/dota_addons/dotacraft/scripts/vscripts/items/auras/vampiric.lua
@@ -5,9 +5,5 @@ LinkLuaModifier("modifier_vampiric_aura", "heroes/dread_lord/vampiric_aura", LUA
 LinkLuaModifier("modifier_vampiric_aura_buff", "heroes/dread_lord/vampiric_aura", LUA_MODIFIER_MOTION_NONE)
 
 function item_scourge_bone_chimes:GetIntrinsicModifierName()
-    if self:GetCaster():HasModifier("modifier_vampiric_aura") then
-        return ""
-    else
-        return "modifier_vampiric_aura"
-    end
+    return "modifier_vampiric_aura"
 end
